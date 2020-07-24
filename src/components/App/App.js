932ecd8f98f26c21c439e0a8f865e217d97b4a69
David Walton <davidwalton6772@gmail.com>
2020-07-24 15:51:48 -0500

    routes created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+import FormFeeling from './FormFeeling/FormFeeling';
+import FormUnderstanding from './FormUnderstanding/FormUnderstanding';
+import FormSupported from './FormSupported/FormSupported';
+import FormSummary from './FormSummary/FormSummary';
+import FormSuccess from './FormSuccess/FormSuccess';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4>
+              <i>Don't forget it!</i>
+            </h4>
+          </header>
+          <br />
+          <Route exact path="/" component={FormFeeling} />
+          <Route exact path="/understanding" component={FormUnderstanding} />
+          <Route exact path="/supported" component={FormSupported} />
+          <Route exact path="/summary" component={FormSummary} />
+          <Route exact path="/success" component={FormSuccess} />
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// String
+// let x = 'Delivery';
+// x = action.payload
+const typeReducer = (state = '', action) => {
+  if (action.type === 'SET_ORDER_TYPE') {
+    return action.payload;
+  } else if (action.type === 'CLEAR_ORDER_TYPE') {
+    return '';
+  }
+
+  return state;
+};
+
+// Object
+/*
+  {
+    fname: STRING,
+    lname: STRING,
+    address: STRING
+  }
+  */
+const customerReducer = (state = {}, action) => {
+  if (action.type === 'SET_CUSTOMER_INFO') {
+    return action.payload;
+  } else if (action.type === 'CLEAR_CUSTOMER_INFO') {
+    return {};
+  }
+
+  return state;
+};
+
+// Array
+const pizzaReducer = (state = [], action) => {
+  if (action.type === 'SET_PIZZA_ORDER') {
+    return action.payload;
+  } else if (action.type === 'CLEAR_PIZZA_ORDER') {
+    return [];
+  }
+
+  return state;
+};
+
+const storeInstance = createStore(combineReducers({}), applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
