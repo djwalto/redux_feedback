5daaa1cf5f05717ebf8a349b9476a99093f63fff
David Walton <davidwalton6772@gmail.com>
2020-08-27 11:01:09 -0500

    updating environment variables from docker for database

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -27,11 +27,11 @@ if (process.env.DATABASE_URL) {
 } else {
   // only change the things on the right side of the ||
   config = {
-    user: process.env.PG_USER || null, //env var: PGUSER
-    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-    database: process.env.DATABASE_NAME || 'prime_feedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+    user: process.env.POSTGRES_USER || null, //env var: PGUSER
+    password: process.env.POSTGRES_PASSWORD || null, //env var: PGPASSWORD
+    host: process.env.POSTGRES_HOST || 'localhost', // Server hosting the postgres database
+    port: process.env.POSTGRES_PORT || 5432, //env var: PGPORT
+    database: process.env.POSTGRES_DB || 'prime_feedback', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
